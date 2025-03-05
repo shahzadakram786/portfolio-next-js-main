aef37e77d012419fea7a7470b5966e01657e8fe8
shahzadakram786 <alyakram786@gmail.com>
2025-03-06 00:05:22 +0500

    navbar completed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
+import NavLink from "./navLink";
 
 const links = [
   { url: "/", title: "Home" },
@@ -15,22 +16,15 @@ const Navbar = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="h-full py-4 flex  justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 ">
-
-            <div className=" hidden md:flex gap-4 w-1/3">
-                {links.map((link) => (
-                    <Link href={link.url} key={link.title}>
-                        {link.title}
-                    </Link>
-                ))}
-
-            </div>
-
-
-
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
+      <div className=" hidden md:flex gap-4 items-center w-1/3">
+        {links.map((link) => (
+         <NavLink link={link} key={link.title}/>
+        ))}
+      </div>
 
       {/* logo  */}
-      <div className="md:hidden lg:flex w-1/3 justify-center">
+      <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center ">
         <Link
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
@@ -43,31 +37,31 @@ const Navbar = () => {
       </div>
 
       {/* social  */}
-            <div className="hidden md:flex gap-4 justify-end w-1/3">
-                <Link href="#">
-                <Image src="/github.png" alt="" width={24} height={24}/>
-                </Link>
-                <Link href="#">
-                <Image src="/linkedin.png" alt="" width={24} height={24}/>
-                </Link>
-                <Link href="#">
-                <Image src="/instagram.png" alt="" width={24} height={24}/>
-                </Link>
-                <Link href="#">
-                <Image src="/facebook.png" alt="" width={24} height={24}/>
-                </Link>
-                <Link href="#">
-                <Image src="/pinterest.png" alt="" width={24} height={24}/>
-                </Link>
+      <div className="hidden md:flex gap-4 justify-end items-center w-1/3">
+        <Link href="#">
+          <Image src="/github.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="#">
+          <Image src="/linkedin.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="#">
+          <Image src="/instagram.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="#">
+          <Image src="/facebook.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="#">
+          <Image src="/pinterest.png" alt="" width={24} height={24} />
+        </Link>
+      </div>
 
-            </div>
-    
       {/* resposicve mentu  */}
       <div className="md:hidden">
         {/* menu button   */}
-        <button className="w-10 h-8  flex flex-col justify-between relative z-50" 
-        onClick={(() => setOpen(!open))}>
-
+        <button
+          className="w-10 h-8  flex flex-col justify-between relative z-50"
+          onClick={() => setOpen(!open)}
+        >
           <div className="w-10 h-1 bg-white rounded"> </div>
           <div className="w-10 h-1 bg-white rounded"></div>
           <div className="w-10 h-1 bg-white rounded"></div>
