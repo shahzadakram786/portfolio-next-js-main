6a37d2e95194354181d3f2efab1d2d9a9e080ecb
shahzadakram786 <alyakram786@gmail.com>
2025-03-07 21:22:59 +0500

    Feed: working on image about us

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 import { motion } from "framer-motion";
+import Brain from "@/components/brain";
+import Image from "next/image";
 
 const AboutPage = () => {
   return (
@@ -12,9 +14,9 @@ const AboutPage = () => {
       transition={{ duration: 1 }}
     >
       {/* container  */}
-      <div className="">
+      <div className="h-full overflow-scroll lg:flex">
         {/* text container  */}
-        <div className="p-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 flex flex-col gap-24 md:gap-12 lg:gap-24">
+        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:w-1/2">
           <div className="flex flex-col justify-center gap-12">
             {/* biography title */}
 
@@ -49,7 +51,6 @@ const AboutPage = () => {
               </svg>
             </div>
 
-
             {/* biograppy scoll svg  */}
             <motion.svg
               initial={{ opacity: 0.2, y: 0 }}
@@ -77,127 +78,215 @@ const AboutPage = () => {
 
           {/* skills container  */}
           <div className="">
+            <div className="flex flex-col justify-center gap-12">
+              {/* Skills title */}
 
-          <div className="flex flex-col justify-center gap-12">
-            {/* Skills title */}
+              <h1 className="text-2xl font-bold">SKILLS</h1>
 
-            <h1 className="text-2xl font-bold">SKILLS</h1>
-
-            {/* skill list  */}
-            <div className="flex gap-4 flex-wrap">
-            <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                JavaScript
+              {/* skill list  */}
+              <div className="flex gap-4 flex-wrap">
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  JavaScript
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  TypeScript
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  React.js
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Next.js
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  SCSS
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Tailwind CSS
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  MongoDB
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  PostgreSQL
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Node.js
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Nest.js
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Express.js
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Spring Boot
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  GraphQL
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Apollo
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Redux
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Framer Motion
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Three.js
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  WebGL
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Webpack
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Vite
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Docker
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  AWS
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Firebase
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Git
+                </div>
+                <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Figma
+                </div>
               </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                TypeScript
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                React.js
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Next.js
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                SCSS
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Tailwind CSS
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                MongoDB
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                PostgreSQL
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Node.js
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Nest.js
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Express.js
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Spring Boot
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                GraphQL
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Apollo
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Redux
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Framer Motion
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Three.js
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                WebGL
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Webpack
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Vite
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Docker
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                AWS
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Firebase
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Git
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Figma
-              </div>
-
+              {/* skill scroll svg  */}
+              <motion.svg
+                initial={{ opacity: 0.2, y: 0 }}
+                animate={{ opacity: 1, y: "10px" }}
+                transition={{
+                  repeat: Infinity,
+                  duration: 3,
+                  ease: "easeInOut",
+                }}
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                width={50}
+                height={50}
+              >
+                <path
+                  d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                  stroke="#000000"
+                  strokeWidth="1"
+                ></path>
+                <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+                <path
+                  d="M15 11L12 14L9 11"
+                  stroke="#000000"
+                  strokeWidth="1"
+                ></path>
+              </motion.svg>
             </div>
-            {/* skill scroll svg  */}
-            <motion.svg
-              initial={{ opacity: 0.2, y: 0 }}
-              animate={{ opacity: 1, y: "10px" }}
-              transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              width={50}
-              height={50}
-            >
-              <path
-                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
-                stroke="#000000"
-                strokeWidth="1"
-              ></path>
-              <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
-              <path
-                d="M15 11L12 14L9 11"
-                stroke="#000000"
-                strokeWidth="1"
-              ></path>
-            </motion.svg>
-
-          </div>
-
           </div>
 
           {/* experiance Container  */}
 
-          <div className="flex flex-col gap-12 justify-center">
-             {/* Expetiace title */}
+          <div className="flex flex-col gap-12 justify-center pb-8">
+            {/* Expetiace title */}
             <h1 className="font-bold text-2xl">Experiance</h1>
+            {/* experiance list  */}
 
+            <div className="">
+              {/* experiave list item  */}
+              <div className="flex justify-between h-48">
+                {/* left  */}
+
+                <div className="w-1/3 ">
+                  {/* job title  */}
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Senior Front end develoepr
+                  </div>
+                  {/* job desc  */}
+                  <div className="p-3 text-sm italic">
+                    MY current employment. Way better than the possition before!
+                  </div>
+                  {/* job Data  */}
+                  <div className="p-3 text-red-300 font-semibold text-sm">
+                    2023 - present
+                  </div>
+                  {/* job compnay  */}
+                  <div className="bg-white p-1 text-sm font-semibold w-fit">
+                    WEBLOOP
+                  </div>
+                </div>
+                {/* center  */}
+                <div className="w-1/6 ">
+                  {/* line */}
+                  <div className=" w-1 h-full bg-gray-600 rounded relative">
+                    {/* line circle  */}
+                    <div className=" -left-2 absolute w-5 h-5 rounded-full ring-red-400 bg-white">
+                      {" "}
+                    </div>
+                  </div>
+                </div>
+                {/* right  */}
+                <div className="w-1/3 "></div>
+              </div>
+
+              {/* 2nd one  */}
+
+              <div className="flex justify-between h-48">
+                {/* left  */}
+
+                <div className="w-1/3 "></div>
+                {/* center  */}
+                <div className="w-1/6 ">
+                  {/* line */}
+                  <div className=" w-1 h-full bg-gray-600 rounded relative">
+                    {/* line circle  */}
+                    <div className=" -left-2 absolute w-5 h-5 rounded-full ring-red-400 bg-white">
+                      {" "}
+                    </div>
+                  </div>
+                </div>
+                {/* right  */}
+                <div className="w-1/3 ">
+                  {/* job title  */}
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Senior Front end develoepr
+                  </div>
+                  {/* job desc  */}
+                  <div className="p-3 text-sm italic">
+                    MY current employment. Way better than the possition before!
+                  </div>
+                  {/* job Data  */}
+                  <div className="p-3 text-red-300 font-semibold text-sm">
+                    2023 - present
+                  </div>
+                  {/* job compnay  */}
+                  <div className="bg-white p-1 text-sm font-semibold w-fit">
+                    WEBLOOP
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+      
+
+      {/* svg container  */}
+      <div className="hidden lg:block sticky top-0 z-30 w-1/3 xl:1/2">
+        {/* <div className="h-1/2 lg:h-full lg:w-1/2 relative"> */}
+          <Image
+            src="/avatar.png"
+            alt="shahzad akram"
+            fill
+            className="object-contain h-full w-full"
+          />
+        {/* </div> */}
+      </div>
       </div>
     </motion.div>
   );
